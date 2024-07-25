0a3630d8f24737d09f1b80c82d6536de62d93ba2
aidan <Aidanjody2.0@gmail.com>
2024-07-24 20:23:23 -0400

    fixed mistype and robot should strafe with left stick x

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/OneDriverTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/OneDriverTeleop.java
@@ -3,6 +3,6 @@ import org.firstinspires.ftc.teamcode.RobotOpMode;
 public class OneDriverTeleop extends RobotOpMode {
     @Override
     public void loop() {
-        dt.robotODrive(gamepad1.left_stick_y, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        dt.robotODrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
     }
 }
