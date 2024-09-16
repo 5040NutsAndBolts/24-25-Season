486fbde849133c9d70563b98581968c8142b2266
jdhar <jdharlow10@gmail.com>
2024-09-16 19:25:11 -0400

    meow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/RichardWheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/RichardWheelTest.java
@@ -1,15 +1,15 @@
 package org.firstinspires.ftc.teamcode.Teleop;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Mechanisms.RichardWheel;
 import org.firstinspires.ftc.teamcode.RobotOpMode;
 
 @TeleOp(name = "Richard Wheel", group = "Teleop")
-public class RichardWheelTest extends RobotOpMode {
+public class RichardWheelTest extends OpMode {
     private RichardWheel wheel;
 
     @Override
     public void init() {
-        super.init();
         wheel = new RichardWheel(hardwareMap);
     }
 
@@ -17,5 +17,8 @@ public class RichardWheelTest extends RobotOpMode {
     public void loop() {
         wheel.spinIn(gamepad1.left_trigger);
         wheel.spinOut(gamepad1.right_trigger);
+        telemetry.addData("Left", gamepad1.left_trigger);
+        telemetry.addData("Right", gamepad1.right_trigger);
+        telemetry.addData("gyatt", wheel.toString());
     }
 }
