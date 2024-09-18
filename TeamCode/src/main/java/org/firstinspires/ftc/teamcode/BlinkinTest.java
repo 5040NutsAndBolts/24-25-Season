44177b53c2d513528d9cbc9eb6760b1ef6be45cc
cttallman <cttallmanchristian@gmail.com>
2024-09-17 21:32:47 -0400

    yay it probably works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlinkinTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.Mechanisms.IntakeTest;
+
+
+@TeleOp(name = "Blinkin", group = "Teleop")
+public class BlinkinTest extends OpMode
+{
+    private IntakeTest test;
+
+
+
+    @Override
+    public void init()
+    {
+         test = new IntakeTest(hardwareMap);
+
+    }
+
+    @Override
+    public void loop() {
+      test.update();
+
+        telemetry.addLine("Red: " + test.colorTrueTest.getRed());
+        telemetry.addLine("Blue: " + test.colorTrueTest.getBlue());
+        telemetry.addLine("Yellow    " + test.colorTrueTest.getYellow());
+        telemetry.addLine("Color Sensor Data (red, green, blue):" +     test.getSensorData());
+    }
+}
+// RAHHHH I NEED TO FIX THIS
