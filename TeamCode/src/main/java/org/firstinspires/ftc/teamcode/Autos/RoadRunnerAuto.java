fba28d257502a158394b781847de9c716c86ce57
cttallman <cttallmanchristian@gmail.com>
2024-10-13 20:40:39 -0400

    worked on odometry ;3 added road runner stuff and made auto roadrunner test.sideways and rotation work. forward does not work and i do not know why.:C

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/RoadRunnerAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/RoadRunnerAuto.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous(name = "Odom Test",group="Autonomous")
 public class RoadRunnerAuto extends AutoOpMode {
-//Sideways and Rotation both work
+//Sideways and Rotation both work :)
     public void loop() {
         odo.robot0Drive(1000,0,0);
         terminateOpModeNow();
