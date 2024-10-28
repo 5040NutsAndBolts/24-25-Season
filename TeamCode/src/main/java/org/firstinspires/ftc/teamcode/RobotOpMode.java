1f911910f5b38a6947134f0381742bbeebe2cda3
jdhar <jdharlow10@gmail.com>
2024-10-28 19:11:51 -0400

    testing wheelintake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOpMode.java
@@ -2,15 +2,22 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.teamcode.Mechanisms.*;
+import org.checkerframework.checker.units.qual.C;
+import org.firstinspires.ftc.teamcode.Mechanisms.Claw;
+import org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain;
+import org.firstinspires.ftc.teamcode.Mechanisms.WheelIntake;
 
 @Disabled
 public class RobotOpMode extends OpMode {
     protected Drivetrain dt;
+    protected WheelIntake wheel;
+    protected Claw claw;
 
     @Override
     public void init() {
         dt = new Drivetrain (hardwareMap);
+        wheel = new WheelIntake(hardwareMap);
+        claw = new Claw(hardwareMap);
     }
     //Leave empty
     @Override public void loop() {}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TwoDriverTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TwoDriverTeleop.java
@@ -6,8 +6,16 @@ public class TwoDriverTeleop extends RobotOpMode {
     @Override
     public void loop() {
         dt.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-        dt.toggleSlowMode(gamepad1.b);
 
-        telemetry.addLine("Slow:  "+dt.isSlow());
+        wheel.lift(gamepad1.left_bumper ? .6 : 0);
+        wheel.lift(gamepad1.right_bumper ? -.6 : 0);
+        //wheel.spinIn (1);//gamepad1.left_trigger >   .8 ? gamepad1.left_trigger : 0);
+        //wheel.spinOut(1);//gamepad1.right_trigger > .8 ? gamepad1.right_trigger : 0);
+
+        wheel.leftServo.setPower(-1);
+        wheel.rightServo.setPower(-1);
+
+        claw.lift(gamepad1.x ? .6 : 0);
+        claw.lift(gamepad1.b ? -.6 : 0);
     }
 }
