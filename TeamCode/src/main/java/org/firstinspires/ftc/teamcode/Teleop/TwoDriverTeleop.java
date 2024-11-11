8b0adbe47b8571f7feeda2ac34e5470307bce143
jdhar <jdharlow10@gmail.com>
2024-11-11 14:31:18 -0500

    Removed deprecated test code, fixed Drivetrain.isSlow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TwoDriverTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TwoDriverTeleop.java
@@ -1,18 +1,14 @@
 package org.firstinspires.ftc.teamcode.Teleop;
-
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.teamcode.RobotOpMode;
+
 @TeleOp (name = "Two Driver", group = "Teleop")
 public class TwoDriverTeleop extends RobotOpMode {
     @Override
     public void loop() {
         dt.drive (gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-
+        dt.toggleSlowMode(gamepad1.x);
         wheel.lift (gamepad2.left_stick_y);
-        telemetry.addLine("wheel power: "+ wheel.spin(gamepad2.left_trigger, gamepad2.right_trigger));
-        telemetry.addLine("lt: " + gamepad2.left_trigger);
-        telemetry.addLine("rt: " + gamepad2.right_trigger);
 
         //claw.liftSlides (gamepad2.right_stick_y);
         //claw.liftClaw(gamepad1.left_bumper ? 1 : gamepad1.right_bumper ? -.2 : 0);
