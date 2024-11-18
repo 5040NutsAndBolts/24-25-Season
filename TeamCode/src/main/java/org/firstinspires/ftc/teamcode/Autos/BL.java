62eb01f73cf1698526e5f0a516eaaf75cdd6eee9
jdhar <jdharlow10@gmail.com>
2024-11-17 20:20:37 -0500

    i forget:)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BL.java
@@ -10,34 +10,9 @@ public class BL extends AutoOpMode {
     }
     @Override
     public void loop() {
-        int t = 0;
-        while((odo.leftE + odo.rightE)/2 < 16000){
-            t++;
-            telemetry.addLine("Moving forward");
-            telemetry.addLine(""+t);
-            telemetry.update();
-            dt.frontLeft.setPower(-.2 + (odo.leftE > odo.rightE ? -.1 : 1) );
-            dt.frontRight.setPower(-.2 + (odo.leftE > odo.rightE ? -.1 : 1) );
-            dt.backLeft.setPower(-.2 + (odo.leftE > odo.rightE ? .1 : -1) );
-            dt.backRight.setPower(-.2 + (odo.leftE > odo.rightE ? .1 : -1) );
-            odo.updatePositionRoadRunner();
-            updateOdoTelemetry();
+        while(odo.curC > -7000) {
+            dt.drive(0,-.3,0);
         }
-        telemetry.addLine("between");
-        telemetry.update();
-        dt.drive(0,0,0);
-        odo.updatePositionRoadRunner();
-        ElapsedTime sideSlamResetTimer = new ElapsedTime();
-        while(odo.centerE < 10000 && sideSlamResetTimer.startTime() < 3){
-            telemetry.addLine("Strafing Left");
-            telemetry.addLine("timer: " + sideSlamResetTimer.startTime());
-            dt.drive(0,-.2,0);
-            odo.updatePositionRoadRunner();
-            updateOdoTelemetry();
-        }
-        telemetry.addLine("between");
-        telemetry.update();
-        dt.drive(0,0,0);
     }
 
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelperClasses/Odometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelperClasses/Odometry.java
@@ -2,131 +2,68 @@ package org.firstinspires.ftc.teamcode.HelperClasses;
 
 import androidx.annotation.NonNull;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class Odometry extends Drivetrain {
-    public double x = 0, y = 0, theta = 0;
-    public DcMotorEx leftOdom, rightOdom, centerOdom;
-    private double leftOdomTraveled, rightOdomTraveled, centerOdomTraveled;
-    private int leftEncoderPos, rightEncoderPos, centerEncoderPos;
-    private static final double ODOM_TICKS_PER_IN = 328.708657034, TRACKWIDTH = 15.5;
-    public int leftE, rightE, centerE;
-    private int leftO, rightO, centerO;
+//EVERYTHING IS IN MILLIMETERS!!!!!!!!!!
+public class Odometry {
+    //Constants
+    //private final static double TICKS_PER_MILLIMETER = 13.19710351; //FB and spin tests
+    //private final static double CENTER_TICKS_PER_MILLIMETER = 12.94128571; //Strafe test
+    //private final static double TRACKWIDTH = 393.7;//distance between left and right wheels
+    //private final static double CENTER_ODO_OFFSET = 152.4;//center wheel offset
 
-    public Odometry(HardwareMap hardwareMap){
-        super(hardwareMap);
-        leftOdom = hardwareMap.get(DcMotorEx.class, "Front Left");
-        rightOdom = hardwareMap.get(DcMotorEx.class, "Front Right");
-        centerOdom = hardwareMap.get(DcMotorEx.class, "Back Left");
-        leftO = leftOdom.getCurrentPosition();
-        rightO = rightOdom.getCurrentPosition();
-        centerO = centerOdom.getCurrentPosition();
+    //Encoders
+    private final DcMotorEx leftOdo, centerOdo, rightOdo;
+    //Leftovers from encoders not starting at zero
+    private final double leftOffset, centerOffset, rightOffset;
+    //Current position
+    //public final Pose currentPose = new Pose(0,0,0);
+    //Previous positions
+    //private double pLeft, pCenter, pRight;
+    public double curL, curC, curR, curLRA;
+
+
+    public Odometry(HardwareMap hardwareMap) {
+        leftOdo = hardwareMap.get(DcMotorEx.class, "Front Left");
+        centerOdo = hardwareMap.get(DcMotorEx.class, "Back Left");
+        rightOdo = hardwareMap.get(DcMotorEx.class, "Front Right");
+        leftOffset = leftOdo.getCurrentPosition();
+        centerOffset = centerOdo.getCurrentPosition();
+        rightOffset = rightOdo.getCurrentPosition();
     }
 
-    private int getDeltaLeftTicks()
-    {
-        try
-        {
-            int total=-leftOdom.getCurrentPosition();
-            int oldPos = leftEncoderPos;
-            leftEncoderPos=total;
-            return oldPos - total;
-        }
-        catch(Exception e)
-        {
-            return 0;
-        }
+    public void update() {
+        curL = leftOdo.getCurrentPosition() - leftOffset;
+        curC = centerOdo.getCurrentPosition() - centerOffset;
+        curR = rightOdo.getCurrentPosition() - rightOffset;
+        curLRA = (curL + curR) / 2.0;
+        /*double deltaLeft = leftOdo.getCurrentPosition() - pLeft - leftOffset;
+        double deltaCenter = centerOdo.getCurrentPosition() - pCenter - centerOffset;
+        double deltaRight = rightOdo.getCurrentPosition() - pRight - rightOffset;
+
+        double distanceLeft = deltaLeft * TICKS_PER_MILLIMETER;
+        double distanceCenter = deltaCenter * CENTER_TICKS_PER_MILLIMETER;
+        double distanceRight = deltaRight * TICKS_PER_MILLIMETER;
+
+        double distanceTheta = (distanceRight - distanceLeft) / TRACKWIDTH;
+        double distanceStrafe = distanceCenter - (CENTER_ODO_OFFSET * distanceTheta);
+        double distanceForward = (distanceRight - distanceLeft) / 2.0;
+
+        currentPose.x+= distanceForward * Math.cos(currentPose.theta) - distanceStrafe * Math.sin(currentPose.theta);
+        currentPose.y+= distanceForward * Math.sin(currentPose.theta) + distanceStrafe * Math.cos(currentPose.theta);
+        currentPose.theta+= distanceTheta;
+
+        pLeft = deltaLeft;
+        pCenter = deltaCenter;
+        pRight = deltaRight;*/
     }
 
-    private int getDeltaRightTicks()
-    {
-        try
-        {
-            int total=rightOdom.getCurrentPosition();
-            int oldPos = rightEncoderPos;
-            rightEncoderPos=total;
-            return oldPos - total;
-        }
-        catch(Exception e)
-        {
-            return 0;
-        }
+    @NonNull
+    @Override
+    public String toString() {
+        return Arrays.toString(new double[] {curL, curC, curR, curLRA});
     }
-
-    private int getDeltaCenterTicks()
-    {
-        try
-        {
-            int total=-centerOdom.getCurrentPosition();
-            int oldPos = centerEncoderPos;
-            centerEncoderPos=total;
-            return oldPos - total;
-        }
-        catch(Exception e)
-        {
-            return 0;
-        }
-    }
-
-    public void updatePositionRoadRunner()
-    {
-        leftE = leftOdom.getCurrentPosition() - leftO;
-        rightE = rightOdom.getCurrentPosition() - rightO;
-        centerE = centerOdom.getCurrentPosition() - centerO;
-        /*try
-        {
-            bulkData = expansionHub.getBulkInputData();
-        }
-        catch(Exception e)
-        {
-            return;
-        }*/
-
-        // Change in the distance (centimeters) since the last update for each odometer
-        double deltaLeftDist = -(getDeltaLeftTicks()/ ODOM_TICKS_PER_IN );
-        double deltaRightDist = -(getDeltaRightTicks()/ ODOM_TICKS_PER_IN);
-        double deltaCenterDist = (getDeltaCenterTicks()/ ODOM_TICKS_PER_IN);
-
-        //adjusts for physical diffrences in pods
-        if(deltaLeftDist < 0)
-            deltaRightDist *= (1.00052425 * 0.99770514);
-        else if (deltaLeftDist > 0)
-            deltaRightDist *= (1.00076168 * 0.99859913);
-
-        leftOdomTraveled += deltaLeftDist;
-        rightOdomTraveled += deltaRightDist;
-        centerOdomTraveled += deltaCenterDist;
-    }
-
-    public void resetOdometry(double x, double y, double theta)
-    {
-        leftO = leftE;
-        rightO = rightE;
-        centerO = centerE;
-
-
-        rightOdomTraveled = 0;
-        leftOdomTraveled = 0;
-
-        leftEncoderPos = 0;
-        rightEncoderPos = 0;
-        centerEncoderPos = 0;
-
-        // Resets encoder values then sets them back to run without encoders because wheels and odometry are same pointer
-        leftOdom.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftOdom.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightOdom.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightOdom.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        centerOdom.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        centerOdom.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-
 }
