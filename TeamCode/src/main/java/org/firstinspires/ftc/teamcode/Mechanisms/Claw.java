e600cfab1f09dffe2f487c85aac9bb62b3b53c0c
jdhar <jdharlow10@gmail.com>
2024-12-11 13:39:17 -0500

    Claw mostly (?) fixed. Most likely still a wire/port issue, needs further testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Claw.java
@@ -6,13 +6,13 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
-	public double rightTrigDown, leftTrigDown;
 	private final Servo pinchServo;
-	private final DcMotor liftMotorTop, liftMotorBottom;
+	private final DcMotor liftMotorTop, liftMotorBottom, rollMotor;
 	public Claw(HardwareMap hardwareMap) {
 		pinchServo = hardwareMap.get(Servo.class, "Pinch Servo");
 
-
+		rollMotor = hardwareMap.get(DcMotor.class, "Roll Motor");
+		rollMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
 		liftMotorTop = hardwareMap.get(DcMotor.class, "Claw Slide Top");
 		liftMotorBottom = hardwareMap.get(DcMotor.class, "Claw Slide Bottom");
@@ -25,13 +25,17 @@ public class Claw {
 		liftMotorTop.setPower(input);
 		liftMotorBottom.setPower(input);
 	}
-	public void pinch(double inOne, double inTwo){
-		double num = inOne - inTwo;
-		if(num < -.05)
-			pinchServo.setPosition(1);
-		else if( num > 0.05) {
-			pinchServo.setPosition(0);
-		}
+	public void pinch(boolean open, boolean closed){
+
+		if(open && pinchServo.getPosition() != 1) pinchServo.setPosition(1);
+		else if(closed) pinchServo.setPosition(0);
+	}
+	public void liftClaw(boolean down, boolean up) {
+		if(down)
+			rollMotor.setPower(-.8);
+		else if(up)
+			rollMotor.setPower(.5);
+		else rollMotor.setPower(0);
 	}
 
 	public int getPosition() {
