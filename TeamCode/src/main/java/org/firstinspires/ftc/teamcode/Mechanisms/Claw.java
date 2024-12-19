532e78b121745821804696532add30117180cbfa
cttallman <cttallmanchristian@gmail.com>
2024-12-18 20:28:27 -0500

    lalalalala commit mechanism fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Claw.java
@@ -6,39 +6,52 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
-	private final Servo pinchServo;
-	private final DcMotor liftMotorTop, liftMotorBottom, rollMotor;
-	public Claw(HardwareMap hardwareMap) {
-		pinchServo = hardwareMap.get(Servo.class, "Pinch Servo");
+    public double rightTrigDown, leftTrigDown;
+    private final Servo pinchServo;
 
-		rollMotor = hardwareMap.get(DcMotor.class, "Roll Motor");
-		rollMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    private final DcMotor liftMotorTop, liftMotorBottom, rollMotor;
 
-		liftMotorTop = hardwareMap.get(DcMotor.class, "Claw Slide Top");
-		liftMotorBottom = hardwareMap.get(DcMotor.class, "Claw Slide Bottom");
-		liftMotorBottom.setDirection(DcMotor.Direction.REVERSE);
-		liftMotorBottom.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-		liftMotorTop.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-	}
+    public Claw(HardwareMap hardwareMap) {
+        pinchServo = hardwareMap.get(Servo.class, "Pinch Servo");
 
-	public void liftSlides(double input) {
-		liftMotorTop.setPower(input);
-		liftMotorBottom.setPower(input);
-	}
-	public void pinch(boolean open, boolean closed){
 
-		if(open && pinchServo.getPosition() != 1) pinchServo.setPosition(1);
-		else if(closed) pinchServo.setPosition(0);
-	}
-	public void liftClaw(boolean down, boolean up) {
-		if(down)
-			rollMotor.setPower(-.8);
-		else if(up)
-			rollMotor.setPower(.5);
-		else rollMotor.setPower(0);
-	}
+        rollMotor = hardwareMap.get(DcMotor.class, "Roll Motor");
+        rollMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-	public int getPosition() {
-		return (liftMotorTop.getCurrentPosition() + liftMotorBottom.getCurrentPosition()) / 2;
-	}
-}
+        liftMotorTop = hardwareMap.get(DcMotor.class, "Claw Slide Top");
+        liftMotorBottom = hardwareMap.get(DcMotor.class, "Claw Slide Bottom");
+    }
+        public void liftSlides(double input){
+            liftMotorTop.setPower(input);
+            liftMotorBottom.setPower(input);
+        }
+
+        public double pinch( double inOne, double inTwo){
+            double num = inOne - inTwo;
+            if (num < -.05)
+                pinchServo.setPosition(1);
+            else if (num > 0.05)
+                pinchServo.setPosition(0);
+
+                return num;
+        }
+
+
+            public void pinch ( boolean open, boolean closed){
+
+                if (open && pinchServo.getPosition() != 1) pinchServo.setPosition(1);
+                else if (closed) pinchServo.setPosition(0);
+            }
+            public void liftClaw ( boolean down, boolean up){
+                if (down)
+                    rollMotor.setPower(-.8);
+                else if (up)
+                    rollMotor.setPower(.5);
+                else rollMotor.setPower(0);
+            }
+
+            public int getPosition() {
+                return (liftMotorTop.getCurrentPosition() + liftMotorBottom.getCurrentPosition()) / 2;
+            }
+        }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TwoDriverTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TwoDriverTeleop.java
@@ -1,20 +1,20 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.RobotOpMode;
-
-import java.util.Arrays;
-
 @TeleOp (name = "Two Driver", group = "Teleop")
 public class TwoDriverTeleop extends RobotOpMode {
     @Override
     public void loop() {
-        dt.drive (gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-
-        wheel.lift (gamepad2.left_stick_y);
-
+        dt.drive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        claw.pinch(gamepad1.right_trigger, gamepad1.left_trigger);
+        wheel.lift(gamepad2.left_stick_y);
+        telemetry.addLine("wheel power: " + wheel.spin(gamepad2.left_trigger, gamepad2.right_trigger));
+        telemetry.addLine("lt: " + gamepad1.left_trigger);
+        telemetry.addLine("rt: " + gamepad1.right_trigger);
+        telemetry.addLine("" + claw.pinch(gamepad1.right_trigger, gamepad1.left_trigger));
         claw.liftSlides (gamepad2.right_stick_y);
         claw.liftClaw(gamepad1.left_bumper, gamepad1.right_bumper);
 
-        claw.pinch(gamepad1.right_trigger > .05, gamepad1.left_trigger > .05);
+
     }
 }
