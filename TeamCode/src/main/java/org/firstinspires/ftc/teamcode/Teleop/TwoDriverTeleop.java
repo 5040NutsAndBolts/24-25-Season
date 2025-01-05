699548cc748d79ad4488da160ae81fd0b381718b
jdhar <jdharlow10@gmail.com>
2025-01-05 01:39:18 -0500

    mergey wergey + added a lil telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TwoDriverTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TwoDriverTeleop.java
@@ -9,6 +9,7 @@ public class TwoDriverTeleop extends RobotOpMode {
     @Override
     public void loop() {
         dt.drive (-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
+        dt.toggleSlowMode(gamepad1.x);
 
         wheel.lift (gamepad2.left_stick_y);
         wheel.spin(gamepad2.right_trigger,gamepad2.left_trigger);
@@ -18,8 +19,7 @@ public class TwoDriverTeleop extends RobotOpMode {
 
         claw.pinch(gamepad1.right_trigger > .05, gamepad1.left_trigger > .05);
 
-        telemetry.addLine("Pinchpos" + claw.pinchServo.getPosition());
-        telemetry.addLine("lrt" + gamepad1.left_trigger +" " + gamepad2.right_trigger);
+        telemetry.addLine("SLOWMODE: " + dt.isSlow());
         telemetry.update();
     }
 }
